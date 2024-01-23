b26917e3ef58a91c0fa0f146ef5622a675660f98
Yash Vijay Bombale <yashbombale@gmail.com>
2024-01-23 18:03:08 +0530

    add useState hook

--- React basics with ts/src/App.tsx
+++ React basics with ts/src/App.tsx
@@ -1,7 +1,40 @@
+import { FormEvent, useState } from "react";
+
+type userType = {
+  name: string;
+  age: number;
+};
+
 function App() {
+  const [user, setUser] = useState<userType>();
+
+  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(user);
+  };
+
   return (
     <>
-      <h1>hey</h1>
+      <form onSubmit={submitHandler}>
+        <input
+          type="number"
+          placeholder="age"
+          value={user?.age || ""}
+          onChange={(e) =>
+            setUser((prev) => ({ ...prev!, age: Number(e.target.value) }))
+          }
+        />
+        <input
+          type="text"
+          placeholder="name"
+          value={user?.name || ""}
+          onChange={(e) =>
+            setUser((prev) => ({ ...prev!, name: e.target.value }))
+          }
+        />
+
+        <button type="submit">Register</button>
+      </form>
     </>
   );
 }
